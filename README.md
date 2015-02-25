1e3ca0d225a46876ef309f0025f4ed5f3570115f
Janne Lehikoinen <janne.lehikoinen@tieto.com>
2015-02-25 21:18:37 +0200

    Added files to project

--- README.md
+++ README.md
@@ -1,2 +1,132 @@
-# media-management-helper
-Automate photo and video management with Dropbox
+Media Management Helper
+=======================
+
+Media Management Helper is part of my personal photo and video management solution. Designed mainly to help automating photo and video management with Dropbox Camera Uploads. See my photo management details [here](http://www.trrt.me/#!./md/photo_management.md).
+
+- Uses compiled Swift binary or  [ExifTool](http://www.sno.phy.queensu.ca/~phil/exiftool/) to read file metadata
+- Creates directory tree based on EXIF DateTimeOriginal year and month metadata information
+- Moves files from source folder to target folder(s)
+
+Pre-requisites
+--------------
+
+- OS X 10.10.X
+- Python 2.7.6
+- exiftool 9.82+ (optional)
+
+Usage
+-----
+
+**Git clone repo:**
+
+`$ git clone ...`
+
+**Edit global variables in `media_mgmt_helper.py` script.**
+
+1. File types
+2. Select Swift binary or ExifTool
+3. Media folder descriptions
+4. Dropbox location
+5. Unsorted folder location
+
+**Run script**
+
+If one common target folder is used for both photos and videos:
+
+`$ ./media_mgmt_helper.py path/to/source_folder path/to/target_media`
+
+If two separate target folders are used, one for photos and one for videos:
+
+`$ ./media_mgmt_helper.py path/to/source_folder path/to/target_photos path/to/target_videos`
+
+Check "Utilizing OS X LaunchAgent" (link) part to automate things..
+
+Additional information
+----------------------
+
+- Root target folder(s) need to exist in advance
+- Script creates target folder structure automatically if needed
+- Files which are missing metadata are moved to `Unsorted media files` folder
+- File types which are not listed in the EXTENSIONS arrays are left in the source folder
+- If no year and month information can't be read from the file, script tries to parse year and month info from file name. Carousel iOS app uses `2015-02-18 08.57.33.jpg` naming convention
+
+Minor things:
+
+- Tool can't handle subfolders inside source folder
+- Tool doesn't check for empty source folder
+
+
+Directory tree naming convention
+--------------------------------
+
+Designed mainly for Unbound iOS app folder view.
+
+**One common target folder for both photos and videos:**
+
+```
+media_root
+  |-year
+      |-year-month
+```
+
+Example:
+
+```
+My Media Files
+  |-2015
+    |-2015-01
+      2015-02
+      etc.
+```
+
+**Two target folders, one for photos and one for videos:**
+
+
+```
+photos_root
+  |-year
+      |-year-month-<photos>
+
+videos_root
+  |-year
+    |-year-month-<videos>
+```
+
+Example:
+
+```
+My Photos
+  |-2015
+    |-2015-01-photos
+      2015-02-photos
+      etc.
+
+My Videos
+  |-2015
+    |-2015-01-videos
+      2015-02-videos
+      etc.
+```
+
+Utilizing OS X LaunchAgent
+--------------------------
+
+OS X LaunchD system can be used with the `media_mgmt_helper.py` script to automatically move files from source folder to target folder(s).
+
+Edit `com.yourdomain.media_mgmt_helper.plist` file with your personal details. Make sure that source path and `WatchPaths` path match.
+
+- `media_mgmt_helper.py` location
+- Source folder
+- Target folder(s)
+- StandardOutPath
+- StandardErrorPath
+
+Move custom LaunchAgent plist to user `LaunchAgents` folder:
+
+`$ mv path/to/com.yourdomain.media_mgmt_helper.plist ~/Library/LaunchAgents/`
+
+Load LaunchAgent:
+
+`$ launchctl load ~/Library/LaunchAgents/com.yourdomain.media_mgmt_helper.plist`
+
+Make sure that `media_mgmt_helper.py` has execute file permissions for LaunchAgent to work properly.
---
+++ media_mgmt_helper.py
@@ -0,0 +1,401 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+media_mgmt_helper.py -- util for moving photo and video files (inside Dropbox)
+
+Copyright (c) 2015 Janne Lehikoinen
+
+This script copies photos and videos from one source folder
+to one target folder or two separate target folders.
+
+Folder(s) are created based on DateTimeOriginal or CreateDate metadata.
+
+"""
+
+"""
+TODO
+- Validate EXT arrays?
+- 'Mass mover' option?
+"""
+
+import os
+import sys
+import datetime
+import subprocess
+import logging
+import re
+
+"""
+EDIT THESE GLOBAL VARIABLES >>>>>
+"""
+
+# 1 - File types
+
+# File types that will be moved to target folder(s)
+PICS_EXTENSIONS = ['.jpg', '.JPG']
+VIDS_EXTENSIONS = ['.mov', '.MOV']
+
+# 2 - Use ExifTool
+
+# Change to True if ExifTool is used
+# Defaults to Swift binary (False)
+USE_EXIFTOOL=False
+
+# 3 - Photo and video subfolder descriptions
+
+# These descriptions are added to subfolder names
+# if photos and videos go to separate folders.
+# Defaults to Finnish
+# English example: PICS_DESC = '-photos' & VIDS_DESC = '-videos'
+PICS_DESC = '-kuvat'
+VIDS_DESC = '-videot'
+
+# 4 - Dropbox folder location
+
+# Defaults to '~/Dropbox'
+DROPBOX_ROOT = os.path.expanduser("~") + '/Dropbox'
+
+# 5 - Unsorted files location
+
+# Defaults to '~/Dropbox/Unsorted Media Files'
+UNSORTED_FOLDER = DROPBOX_ROOT + '/Unsorted Media Files'
+
+# Optional customization >>>>>
+
+# Binary paths
+EXIFTOOL = '/usr/bin/exiftool'
+SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
+SWIFT_TOOL = SCRIPT_PATH + '/get-metadata'
+
+# Year & month regex
+REGEX_YEAR = r'^20\d\d$'
+REGEX_MONTH = r'^(0?[1-9]|1[012])$'
+
+# Date&time format in log
+TIME_FORMAT = '%d.%m.%Y %H.%M.%S'
+
+"""
+>>>>> END GLOBAL VARIABLE EDITING
+"""
+
+# Helper functions
+
+def msg_info(msg):
+    """Print date & time stamp and message"""
+
+    date_pattern = datetime.datetime.now().strftime(TIME_FORMAT)
+    print '%s: %s' % (date_pattern, msg)
+
+
+def msg_error(msg):
+    """Print error message"""
+
+    print 'ERROR: %s' % msg
+
+
+def msg_usage():
+    """Print usage message"""
+
+    print """Usage:
+
+    If one common target folder is used for both photos and videos:
+    ./media_mgmt_helper.py path/to/source_folder path/to/target_media
+
+    If two separate target folders are used, one for photos and one for videos:
+    ./media_mgmt_helper.py path/to/source_folder path/to/target_photos path/to/target_videos
+    """
+
+
+def preflight_checks():
+    """Run few preflight checks"""
+
+    if USE_EXIFTOOL:
+        # Check that ExifTool is installed
+        if not os.path.exists(EXIFTOOL):
+            msg_error('ExifTool not found!')
+            sys.exit(1)
+    else:
+        # Check that custom Swift binary is in place
+        if not os.path.exists(SWIFT_TOOL):
+            msg_error('Swift binary not found!')
+            sys.exit(1)
+
+    # Create UNSORTED_FOLDER if it doesn't exist
+    if not os.path.exists(UNSORTED_FOLDER):
+        msg_info('Creating new folder: ' + UNSORTED_FOLDER)
+        os.makedirs(UNSORTED_FOLDER)
+
+
+def folder_validator(folder):
+    """
+    Validate folders and add trailing slash if needed
+    Return: full path
+    """
+
+    if not os.path.exists(folder):
+        msg_error('Folder ' + folder + ' not found!')
+        sys.exit(1)
+
+    if not os.path.isdir(folder):
+        msg_error(folder + ' is not a folder!')
+        sys.exit(1)
+
+    # Add trailing slash to folder if not found
+    if not folder.endswith(os.path.sep):
+        folder += os.path.sep
+
+    return folder
+
+
+def get_model_exiftool(file_path):
+    """
+    Get camera model using ExifTool
+    Return: model (e.g. iPhone):
+    """
+
+    command = [EXIFTOOL,
+               '-s', '-s', '-s',
+               '-model', file_path]
+    model = call_cmd(command)
+
+    return model
+
+
+def get_year_month_exiftool(file_path):
+    """
+    Get year and month from CreateDate tag using ExifTool
+    If no CreateDate tag, parse year and month from file name
+    Carousel naming convention: 2015-02-12 00.05.58.jpg
+    Return: year, month (e.g. 2015 and 02)
+    """
+
+    file_name = os.path.basename(file_path)
+    command = [EXIFTOOL,
+               '-s', '-s', '-s',
+               '-createDate', file_path]
+    date = call_cmd(command)
+
+    if date:
+        # Parse year and month
+        year = date[:4]
+        month = date[5:7]
+    else:
+        # Try parsing year and month from file name
+        year = file_name[:4]
+        month = file_name[5:7]
+
+    # Check that year has valid value
+    if not re.match(REGEX_YEAR, year):
+        year = ''
+
+    # Check that month has valid value
+    if not re.match(REGEX_MONTH, month):
+        month = ''
+
+    return year, month
+
+
+def get_model_swift(file_path):
+    """
+    Get camera model using Swift
+    Return: model (e.g. iPhone):
+    """
+    file_name_dummy, extension = os.path.splitext(file_path)
+    model = ""
+
+    if extension in PICS_EXTENSIONS:
+        command = [SWIFT_TOOL, 'photo', 'Model', file_path]
+        model = call_cmd(command)
+    elif extension in VIDS_EXTENSIONS:
+        command = [SWIFT_TOOL, 'video', 'common/model', file_path]
+        model = call_cmd(command)
+
+    return model
+
+def get_year_month_swift(file_path):
+
+    """
+    Get year and month from DateTimeOriginal metadata using Swift
+    If no Create Date tag, parse year name from file name
+    Carousel naming convention: 2015-02-12 00.05.58.jpg
+    Return: year and month (e.g. 2015 and 02)
+    """
+    file_name_dummy, extension = os.path.splitext(file_path)
+    file_name = os.path.basename(file_path)
+
+    if extension in PICS_EXTENSIONS:
+        command = [SWIFT_TOOL, 'photo', 'DateTimeOriginal', file_path]
+        date = call_cmd(command)
+    elif extension in VIDS_EXTENSIONS:
+        command = [SWIFT_TOOL, 'video', 'common/creationDate', file_path]
+        date = call_cmd(command)
+
+    if date:
+        # Parse year and month
+        year = date[:4]
+        month = date[5:7]
+    else:
+        # Try parsing year and month from file name
+        year = file_name[:4]
+        month = file_name[5:7]
+
+    # Check that year has valid value
+    if not re.match(REGEX_YEAR, year):
+        year = ''
+
+    # Check that month has valid value
+    if not re.match(REGEX_MONTH, month):
+        month = ''
+
+    return year, month
+
+def call_cmd(command):
+    """
+    Subprocess function that runs cli commands
+    Return: stdout (white space removed)
+    """
+
+    task = subprocess.Popen(command,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    out, err = task.communicate()
+    task.wait()
+
+    if err:
+        msg_error(err)
+
+    # Strip whitespace from cmd output
+    return out.strip()
+
+
+def create_dir_tree(target_path, year, month, media_type):
+    """
+    Create directory tree if it doesn't exist
+    Return: full target path (e.g. path/to/target/2015/2015-02)
+    """
+
+    year_path = target_path + year
+    month_path = year_path + '/' + year + \
+        '-' + month + media_type
+
+    if not os.path.exists(year_path):
+        msg_info('Creating new folder: ' + year_path)
+        os.makedirs(year_path)
+    if not os.path.exists(month_path):
+        msg_info('Creating new folder: ' + month_path)
+        os.makedirs(month_path)
+
+    return month_path
+
+
+def move_file(source_path, target_path, file_name, model):
+    """
+    Move file to target location
+    Don't move file if found in target folder
+    """
+
+    last_path = os.path.basename(os.path.normpath(target_path))
+
+    # Construct model string for logging
+    if model:
+        model = model + ' - '
+
+    # Double check for existing target files
+    if os.path.isfile(os.path.join(target_path, file_name)):
+        msg_info(file_name + ' already exists in ' +
+                 last_path + ' folder so it remains in place')
+    else:
+        try:
+            # Actual move operation
+            msg_info(model + 'Moving ' +
+                     file_name + ' to ' + last_path)
+            os.rename(os.path.join(source_path, file_name),
+                      os.path.join(target_path, file_name))
+        except OSError as e:
+            msg_error(e)
+        except IOError as e:
+            msg_error(e.strerror)
+
+
+def main():
+
+    # Preflight checks
+    preflight_checks()
+
+    # Vars
+    full_paths = []
+    one_target_folder = False
+
+    # Check cmd line arguments except the script itself
+    args = sys.argv[1:]
+
+    if len(args) == 0:
+        msg_usage()
+        sys.exit(1)
+
+    if len(args) < 2:
+        msg_error('Too few arguments!')
+        msg_usage()
+        sys.exit(1)
+
+    elif len(args) == 2:
+        one_target_folder = True
+
+    elif len(args) > 3:
+        msg_error('Too many arguments!')
+        msg_usage()
+        sys.exit(1)
+
+    # Validate folders and add trailing slash
+    for arg in args:
+        full_paths.append(folder_validator(arg))
+
+    # Main loop
+    for file in os.listdir(full_paths[0]):
+
+        full_source_path = full_paths[0] + file
+
+        # Go through all valid file extensions
+        ALL_EXTENSIONS = PICS_EXTENSIONS + VIDS_EXTENSIONS
+        if any(file.endswith(x) for x in ALL_EXTENSIONS):
+
+            # Get year and month from file metadata
+            if USE_EXIFTOOL:
+                model = get_model_exiftool(full_source_path)
+                year, month = get_year_month_exiftool(full_source_path)
+            else:
+                model = get_model_swift(full_source_path)
+                year, month = get_year_month_swift(full_source_path)
+
+            # Check for non-empty strings
+            if year and month:
+
+                # Create folder structure
+                # Handle one or two target folders
+                if one_target_folder:
+                    target_path = create_dir_tree(full_paths[1], year,
+                                                  month, media_type='')
+                else:
+                    # Handle photo vs. video file types
+                    if any(file.endswith(x) for x in PICS_EXTENSIONS):
+                        target_path = create_dir_tree(full_paths[1],
+                                                      year,
+                                                      month,
+                                                      media_type=PICS_DESC)
+                    elif any(file.endswith(x) for x in VIDS_EXTENSIONS):
+                        target_path = create_dir_tree(full_paths[2],
+                                                      year,
+                                                      month,
+                                                      media_type=VIDS_DESC)
+                # Move files
+                move_file(full_paths[0], target_path, file, model)
+
+            else:
+                msg_info(file + ' is missing Create Date tag')
+                # Move files to UNSORTED_FOLDER
+                move_file(full_paths[0], UNSORTED_FOLDER, file, model)
+    # main loop ends
+
+if __name__ == "__main__":
+    main()
